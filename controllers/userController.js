be002a3aafe38a1727a10351e7a58a2dfcdcee93
sanaullah-nexquery <sanaullah.rasheed@nexquery.com>
2024-12-25 12:39:33 +0500

    contiue

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const asyncHandler = require("express-async-handler");
+//@desc Register a user
+// @route  POST /api/users/register
+// @access public
+const registerUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Register the user" });
+});
+
+//@desc Login  user
+// @route  POST /api/users/login
+// @access public
+const loginUser = asyncHandler(async (req, res) => {
+  res.json({ message: "login user" });
+});
+
+//@desc   user info
+// @route  POST /api/users/current
+// @access private
+const currentUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Current User Information" });
+});
+
+module.exports = { registerUser, loginUser, currentUser };
