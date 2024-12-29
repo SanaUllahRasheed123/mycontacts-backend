2406c98764013316a452ad54b6169a1ba732a97a
sanaullah-nexquery <sanaullah.rasheed@nexquery.com>
2024-12-29 16:21:32 +0500

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,23 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 const asyncHandler = require("express-async-handler");
 //@desc Register a user
 // @route  POST /api/users/register
 // @access public
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    throw new Error("User already registered!");
+  }
+
+  // Hash Password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password", hashedPassword);
   res.json({ message: "Register the user" });
 });
 
