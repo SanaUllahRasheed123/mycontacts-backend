2261befd53be5797e8d19c5e556281d72ba6a296
sanaullah-nexquery <sanaullah.rasheed@nexquery.com>
2024-12-30 15:14:15 +0500

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 //@desc Register a user
 // @route  POST /api/users/register
@@ -18,6 +19,19 @@ const registerUser = asyncHandler(async (req, res) => {
   // Hash Password
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log("Hashed Password", hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  console.log(`User created ${user}`);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
+
   res.json({ message: "Register the user" });
 });
 
@@ -25,10 +39,34 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  POST /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const user = await User.findOne({ email });
+  // compare password with hashedpassword
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password is not valid");
+  }
+  // res.json({ message: "login user" });
 });
 
-//@desc   user info
+//@desc  Current user info
 // @route  POST /api/users/current
 // @access private
 const currentUser = asyncHandler(async (req, res) => {
