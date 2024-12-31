d5f7bf400580adec28925a6407bffabcbe1affe4
sanaullah-nexquery <sanaullah.rasheed@nexquery.com>
2024-12-31 12:44:48 +0500

    updated

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -22,6 +22,7 @@ const createContact = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
+    user_id: req.user.id,
   });
   res.status(201).json(contact);
 });
@@ -48,6 +49,10 @@ const updateContact = asyncHandler(async (req, res) => {
     throw new Error("Contact not found");
   }
 
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contact");
+  }
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -60,13 +65,17 @@ const updateContact = asyncHandler(async (req, res) => {
 // @route  DELETE /api/contacts/:id
 // @access private
 const deleteContact = asyncHandler(async (req, res) => {
-  const contact = await Contact.findById(req.params.id);
+  const contact = await Contact.findById({ user_id: req.params.id });
   if (!contact) {
     res.status(404);
     throw new Error("Contact not found");
   }
 
-  await Contact.deleteOne();
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contact");
+  }
+  await Contact.deleteOne({ _id: req.params.id });
 
   res.status(200).json(contact);
 });
